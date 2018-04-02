6cb38a0071baad67f1e42183f6738a132ee7537c
szwiec <kuba.szwiec@gmail.com>
2018-04-02 19:47:46 +0100

    FilesProvider is Companion ocject now

--- app/src/main/java/eu/szwiec/replayview/replay/FilesProvider.kt
+++ app/src/main/java/eu/szwiec/replayview/replay/FilesProvider.kt
@@ -13,50 +13,49 @@ class FilesProvider private constructor() {
     companion object {
 
         fun getFiles(zipPath: String, extensions: List<String>): List<File> {
-            val provider = FilesProvider()
-            val extractedDir = provider.getExtractedDir(zipPath)
+            val extractedDir = extractDir(zipPath)
 
+            return getMatchingFiles(extensions, extractedDir)
+        }
 
-            return provider.getMatchingFiles(extensions, extractedDir)
+        private fun extractDir(zipPath: String): File {
+            val destinationPath = getDestinationPath(zipPath)
+            val isUnzipSuccessful = FileUtils.unzip(zipPath, destinationPath)
+
+            if (!isUnzipSuccessful) throw RuntimeException("Problem with unzipping")
+
+            return File(destinationPath)
+        }
+
+        private fun getDestinationPath(zipPath: String): String {
+            val filenameWithExtension = zipPath.substring(zipPath.lastIndexOf("/") + 1)
+            return SDKConstants.SDCARD_PATH + FilenameUtils.removeExtension(filenameWithExtension)
+        }
+
+        private fun getMatchingFiles(types: List<String>, dir: File): List<File> {
+            val files = ArrayList<File>()
+
+            for (type in types) {
+                files.addAll(getMatchingFiles(type, dir))
+            }
+            return files
+        }
+
+        private fun getMatchingFiles(type: String, dir: File): List<File> {
+            val files = ArrayList<File>()
+
+            val names = dir.list { d, name ->
+                val regex = String.format(".*\\.%s\\d+", type)
+                name.matches(regex.toRegex())
+            }
+
+            for (name in names) {
+                val file = File(dir.toString() + "/" + name)
+                files.add(file)
+            }
+
+            return files
         }
     }
 
-    private fun getExtractedDir(zipPath: String): File {
-        val destinationPath = getDestinationPath(zipPath)
-        val isUnzipSuccessful = FileUtils.unzip(zipPath, destinationPath)
-
-        if (!isUnzipSuccessful) throw RuntimeException("Problem with unzipping")
-
-        return File(destinationPath)
-    }
-
-    private fun getDestinationPath(zipPath: String): String {
-        val filenameWithExtension = zipPath.substring(zipPath.lastIndexOf("/") + 1)
-        return SDKConstants.SDCARD_PATH + FilenameUtils.removeExtension(filenameWithExtension)
-    }
-
-    private fun getMatchingFiles(types: List<String>, dir: File): List<File> {
-        val files = ArrayList<File>()
-
-        for (type in types) {
-            files.addAll(getMatchingFiles(type, dir))
-        }
-        return files
-    }
-
-    private fun getMatchingFiles(type: String, dir: File): List<File> {
-        val files = ArrayList<File>()
-
-        val names = dir.list { d, name ->
-            val regex = String.format(".*\\.%s\\d+", type)
-            name.matches(regex.toRegex())
-        }
-
-        for (name in names) {
-            val file = File(dir.toString() + "/" + name)
-            files.add(file)
-        }
-
-        return files
-    }
 }
