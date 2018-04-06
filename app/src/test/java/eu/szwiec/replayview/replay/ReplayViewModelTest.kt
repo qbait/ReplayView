c7f8b011a9367a74b9b3a678cbcdc375a482d6d0
szwiec <kuba.szwiec@gmail.com>
2018-04-06 11:26:37 +0100

    the first unit test on ViewModel

--- app/src/test/java/eu/szwiec/replayview/replay/ReplayViewModelTest.kt
+++ app/src/test/java/eu/szwiec/replayview/replay/ReplayViewModelTest.kt
@@ -1,29 +1,40 @@
 package eu.szwiec.replayview.replay
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
 import eu.szwiec.replayview.FilesProvider
-import eu.szwiec.replayview.utils.NonNullLiveData
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
 
 class ReplayViewModelTest {
-    @Mock private lateinit var filesProviderMock: FilesProvider
-    @Mock private lateinit var isPlayingLDMock: NonNullLiveData<Boolean>
+    @Mock
+    private lateinit var filesProviderMock: FilesProvider
+    @Mock
+    private lateinit var observer: Observer<Boolean>
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
 
     private lateinit var viewModel: ReplayViewModel
 
     @Before
     fun init() {
         MockitoAnnotations.initMocks(this)
-        //viewModel = ReplayViewModel(filesProviderMock, speeds)
+        viewModel = ReplayViewModel(filesProviderMock)
     }
 
     @Test
     fun testTogglePlayPause() {
-//        Mockito.`when`(isPlayingLDMock.value).thenReturn(false)
-//        viewModel.togglePlayPause()
-//        Mockito.verify(isPlayingLDMock.value).compareTo(true)
+        viewModel.isPlayingLD.observeForever(observer)
+        viewModel.togglePlayPause()
+        verify(observer).onChanged(true)
+        viewModel.togglePlayPause()
+        verify(observer).onChanged(false)
     }
 }
